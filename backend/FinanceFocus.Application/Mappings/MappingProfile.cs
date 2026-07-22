4b6708d5528a0d2cd7ee4599aa68554c7ae0a651
Aygen5 <aygenyildirim27@gmail.com>
2026-07-22 18:21:08 +0300

    feat(application): add AutoMapper MappingProfile for entity-dto mappings

---
+++ backend/FinanceFocus.Application/Mappings/MappingProfile.cs
@@ -0,0 +1,48 @@
+using AutoMapper;
+using FinanceFocus.Application.DTOs.ActivityLogs;
+using FinanceFocus.Application.DTOs.AI;
+using FinanceFocus.Application.DTOs.Budgets;
+using FinanceFocus.Application.DTOs.FinancialHealth;
+using FinanceFocus.Application.DTOs.Forecast;
+using FinanceFocus.Application.DTOs.Goals;
+using FinanceFocus.Application.DTOs.Notifications;
+using FinanceFocus.Application.DTOs.Portfolio;
+using FinanceFocus.Application.DTOs.Subscriptions;
+using FinanceFocus.Application.DTOs.Transactions;
+using FinanceFocus.Domain.Entities;
+
+namespace FinanceFocus.Application.Mappings;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<Transaction, TransactionDto>();
+        CreateMap<CreateTransactionDto, Transaction>();
+        CreateMap<UpdateTransactionDto, Transaction>();
+
+        CreateMap<Budget, BudgetDto>()
+            .ForMember(dest => dest.SpentAmount, opt => opt.Ignore());
+        CreateMap<CreateBudgetDto, Budget>();
+        CreateMap<UpdateBudgetDto, Budget>();
+
+        CreateMap<Goal, GoalDto>();
+        CreateMap<CreateGoalDto, Goal>();
+        CreateMap<UpdateGoalDto, Goal>();
+
+        CreateMap<PortfolioAsset, PortfolioAssetDto>();
+        CreateMap<CreatePortfolioAssetDto, PortfolioAsset>();
+        CreateMap<UpdatePortfolioAssetDto, PortfolioAsset>();
+
+        CreateMap<Subscription, SubscriptionDto>();
+        CreateMap<CreateSubscriptionDto, Subscription>();
+        CreateMap<UpdateSubscriptionDto, Subscription>();
+
+        CreateMap<Notification, NotificationDto>();
+        CreateMap<ActivityLog, ActivityLogDto>();
+        CreateMap<AIConversation, AIConversationDto>();
+        CreateMap<AIConversation, ChatMessageDto>();
+        CreateMap<ForecastHistory, ForecastDto>();
+        CreateMap<FinancialHealthHistory, FinancialHealthDto>();
+    }
+}
--- backend/FinanceFocus.Application/obj/Debug/net10.0/FinanceFocus.Application.AssemblyInfo.cs
+++ backend/FinanceFocus.Application/obj/Debug/net10.0/FinanceFocus.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinanceFocus.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7f55c8e3cf57e021c4025008b1f15667d1a6ae3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4b69419335595cc960ee28fba08b2adb7e7aab1")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinanceFocus.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinanceFocus.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
